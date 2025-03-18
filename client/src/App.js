0f695904b6ff118e78183bf28d7dbe7ae423b483
rxshxni <roshaninp33@gmail.com>
2025-03-18 01:33:01 -0400

    ClickOn/New Page
    Should take you to a new page when either buttons clicked on login page!

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import logo from './images/RuLogo.png';
 import background from './images/background.png'; 
 
-function App() {
+function Home() {
   return (
     <div 
       className="App"
@@ -18,11 +19,35 @@ function App() {
         <img className="mt-5" src={logo} alt="RUHousing Logo" style={{ width: '250px', height: '230px' }}/>
       </div>
       <div className="margin-top"> 
+        <Link to="/new-user">
         <button className="btn btn-primary">RU A NEW USER</button>
+        </Link>
         <br/>
+        <Link to="/landlord">
         <button className="btn btn-primary mt-2">RU Landlord</button>
+        </Link>
       </div>
     </div>
+  );
+}
+
+function NewUser() {
+  return <h2 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome, New User!</h2>;
+}
+
+function Landlord() {
+  return <h2 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome, Landlord!</h2>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new-user" element={<NewUser />} />
+        <Route path="/landlord" element={<Landlord />} />
+      </Routes>
+    </Router>
   );
 }
 
