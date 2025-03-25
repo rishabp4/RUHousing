86475d1a882e6df6823ef3e342940df94e97d2c1
Nurullah Dogan <nurullahdogan515@gmail.com>
2025-03-25 01:26:58 -0400

    Added Homepage, Temporary login bypass button, images, and updated App/Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import './App.css';
-import Home from './Home';
-import NewUser from './NewUser';
-import Login from './Login';
-import Student from './Student';
-import Landlord from './Landlord';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import NewUser from "./NewUser";
+import Login from "./Login";
+import Student from "./Student";
+import Landlord from "./Landlord";
+import HomePage from "./HomePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/new-user" element={<NewUser />} />
         <Route path="/login" element={<Login />} />
         <Route path="/student" element={<Student />} />
         <Route path="/landlord" element={<Landlord />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
