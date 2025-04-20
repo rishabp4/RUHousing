75035196d0e61800c0bb74b05a134762942caa1f
Venesha09 <va285@scarletmail.rutgers.edu>
2025-04-19 23:20:33 -0400

    server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
- const cors = require('cors');
- 
- const app = express();
- app.use(cors());           // Enable CORS
- app.use(express.json());   // Parse JSON bodies
- 
- // Simple test route
- app.get('/', (req, res) => {
-   res.send('Hello from the RUHousing Express server!');
- });
- 
- // Start server on port 5000
- const PORT = process.env.PORT || 5001;
- app.listen(PORT, () => {
-   console.log(`Server is running on http://localhost:${PORT}`);
- });
+const cors = require('cors');
+const app = express();
+app.use(cors());           // Enable CORS
+app.use(express.json());   // Parse JSON bodies
+
+
+app.get('/', (req, res) => {
+res.send('Hello from the RUHousing Express server!');
+
+
+});
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+console.log(`Server is running on http://localhost:${PORT}`);
+});
